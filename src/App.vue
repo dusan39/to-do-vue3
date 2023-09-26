88f491e7a60c9f0169dcee714593e4e41f95783d
Dusan <dusanrajkovic39@gmail.com>
2023-09-26 20:09:15 +0200

    Stable versione

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,125 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name">
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TO-DO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your to-do list?</h4>
+        <input 
+          type="text" 
+          placeholder="e.g. make a video" 
+          v-model="inputContent" />
+
+          <h4>Pick a category</h4>
+
+          <div class="options">
+            <label>
+              <input 
+                type="radio" 
+                name="category" 
+                value="business"
+                v-model="inputCategory" />
+              <span class="bubble business"></span>
+              <div>Business</div>
+            </label>
+
+            <label>
+              <input 
+                type="radio" 
+                name="category" 
+                value="personal"
+                v-model="inputCategory" />
+              <span class="bubble personal"></span>
+              <div>Personal</div>
+            </label>
+          </div>
+
+          <input type="submit" value="Add to-do" />
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TO-DO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todosAsc" :class="`todo-item ${todo.done && 'done'}`">
+
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main>
+
 </template>
 
+<script setup>
+
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const inputContent = ref('')
+const inputCategory = ref(null)
+
+  const todosAsc = computed(() => todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt
+  }))
+
+  const addTodo = () =>{
+    if(inputContent.value.trim() === '' || inputCategory.value === null){
+      return
+    }
+
+    todos.value.push({
+      content: inputContent.value,
+      category: inputCategory.value,
+      done: false,
+      createdAt: new Date().getTime()
+    })
+
+    inputContent.value = ''
+    inputCategory.value = null
+  }
+
+  const removeTodo = todo =>{
+    todos.value = todos.value.filter(t => t !== todo)
+  }
+
+  watch(todos, newVal =>{
+    localStorage.setItem('todos', JSON.stringify(newVal))
+  }, { deep:true }) // deep controlla che anche un singolo contenuto dell'array sia cambiato
+
+  watch(name, (newVal) => {
+    localStorage.setItem('name', newVal)
+  })
+
+  onMounted(() =>{
+    name.value = localStorage.getItem('name') || ''
+    todos.value = JSON.parse(localStorage.getItem('todos')) || []
+  })
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
